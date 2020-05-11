454f391b2c4c2254bf7c08860ef19bb35343b5fd
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2020-05-11 18:32:53 -0400

    Add user profile pic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ def update
 	@user = User.find(params[:id])
 	if @user.update(user_params)
 		flash[:notice] = "Your account information was successfully updated"
-		redirect_to articles_path
+		redirect_to @user
 	else
 		render 'edit'
 	end
@@ -32,6 +32,10 @@ end
 def show
 	@user = User.find(params[:id])
 	@articles = @user.articles
+end
+
+def index
+	@users = User.all
 end
 
 
