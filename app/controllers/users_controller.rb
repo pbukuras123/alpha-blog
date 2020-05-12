0d86ec3e8c9d922d548ecdda0a6e66470e4da0f8
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2020-05-11 22:08:24 -0400

    Add Admin features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,18 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
+	before_action :require_user, only: [:edit, :update]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 
 def new
 	@user = User.new
 end
 
-def edit
-	@user = User.find(params[:id])
+def edit	
 end
 
 def update
-	@user = User.find(params[:id])
-	if @user.update(user_params)
+	if @user = User.find(params[:id])
 		flash[:notice] = "Your account information was successfully updated"
 		redirect_to @user
 	else
@@ -22,6 +23,7 @@ end
 def create
 	@user = User.new(user_params)
 	if @user.save
+		session[:user_id] = @user.id
 		flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
 		redirect_to articles_path
 	else
@@ -30,12 +32,19 @@ def create
 end
 
 def show
-	@user = User.find(params[:id])
-	@articles = @user.articles
+	
+	@articles = @user.articles.paginate(page: params[:page], per_page: 5)
 end
 
 def index
-	@users = User.all
+	@users = User.paginate(page: params[:page], per_page: 5)
+end
+
+def destroy
+	@user.destroy
+	session[:user_id] = nil if @user == current_user
+	flash[:notice] = "Account and all associated articles successfully deleted"
+	redirect_to root_path
 end
 
 
@@ -43,6 +52,17 @@ private
 
 def user_params
 	params.require(:user).permit(:username, :email, :password)
+end
+
+def set_user
+	@user = User.find(params[:id])
+end
+
+def require_same_user
+	if current_user != @user && !current_user.admin?
+		flash[:alert] = "You can only edit or delete your own profile"
+		redirect_to @user
+	end
 end
 
 end
